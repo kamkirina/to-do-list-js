8e7a13607335e87bde458a8c152d3db8be6aae37
Irina <70759778+kamkirina@users.noreply.github.com>
2023-02-10 12:40:33 +0300

    Add comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
-
+//Получаем элементы для вставки новых задач
 const taskList = document.querySelector(".tasks-list");
 const taskForm = document.querySelector(".create-task-block");
 
+
+//Массив объектов уже существующих задач
 const tasks = [
   {
     id: "1138465078061",
@@ -21,6 +23,8 @@ const tasks = [
   },
 ];
 
+
+//Формируем новую задачу
 const addNewTask = (text) => {
   const taskId = Date.now().toString();
   const task = {
@@ -29,10 +33,12 @@ const addNewTask = (text) => {
     text
   };
 
-  tasks.push(task);
-  taskList.append(createTaskItem(text, taskId));
+  tasks.push(task); //записываем задачу в массив
+  taskList.append(createTaskItem(text, taskId)); //выводим новую задачу в список
 };
 
+
+//формируем HTML разметку под список задач
 function createTaskItem(task, taskIds) {
   const taskItem = document.createElement("div");
   taskItem.dataset.taskId = taskIds;
@@ -74,25 +80,31 @@ function createTaskItem(task, taskIds) {
   return taskItem;
 }
 
+
+//выводим массив задач в созданную разметку
 tasks.forEach((el) => {
   taskList.append(createTaskItem(el.text, el.id));
 });
 
+
+//создаем разметку для вывода ошибки
 const createErrorMessage = (taskText) => {
   const errorMessage = document.createElement("span");
   errorMessage.className = "error-message-block";
+  // если поле воода пустое
   if (!taskText) {
     errorMessage.remove();
     errorMessage.textContent = "Название задачи не должно быть пустым";
     taskForm.append(errorMessage);
     return errorMessage;
   } else {
-    errorMessage.textContent = "Задача с таким названием уже существует";
+    errorMessage.textContent = "Задача с таким названием уже существует"; // если поле не пустое
     taskForm.append(errorMessage);
     return errorMessage;
   }
 };
 
+//обработка ввода задачи - кнопка нажать или Enter
 taskForm.addEventListener("submit", (event) => {
   const errorMessage = document.querySelector(".error-message-block");
   event.preventDefault();
@@ -114,6 +126,8 @@ taskForm.addEventListener("submit", (event) => {
   }
 });
 
+
+//Разметка для модального окна
 const createModal = document.createElement("div");
 createModal.innerHTML =
   '<div class="modal-overlay modal-overlay_hidden"><div class="delete-modal"><h3 class="delete-modal__question">Вы действительно хотите удалить эту задачу?</h3><div class="delete-modal__buttons"><button class="delete-modal__button delete-modal__cancel-button">Отмена</button><button class="delete-modal__button delete-modal__confirm-button">Удалить</button></div></div></div>';
@@ -121,38 +135,42 @@ document.body.prepend(createModal);
 
 const modalDelete = document.querySelector(".modal-overlay");
 
+//Делегирование событий - один обработчик на все кнопки удалить
 taskList.addEventListener("click", (event) => {
   const isButton = event.target.closest("button");
   const chosenTask = event.target.closest(".task-item");
-  const dataTaskId = chosenTask.dataset.taskId;
-  console.log(dataTaskId);
+  const dataTaskId = chosenTask.dataset.taskId; //получение Id выбранной задачи
+  
   if (isButton) {
-    modalDelete.classList.remove("modal-overlay_hidden");
-    const modalButtonCancel = document.querySelector(
-      ".delete-modal__cancel-button"
-    );
+    modalDelete.classList.remove("modal-overlay_hidden"); //показываем модальное окно
+    
+    //обработка нажатия на кнопку отмена в модальном окне
+    const modalButtonCancel = document.querySelector(".delete-modal__cancel-button");
+       
     modalButtonCancel.addEventListener("click", () =>
-      modalDelete.classList.add("modal-overlay_hidden")
-    );
-    const modalButtonConfirm = document.querySelector(
-      ".delete-modal__confirm-button"
-    );
+      modalDelete.classList.add("modal-overlay_hidden"));
+    
+    //обработка нажатия на кнопку подтвердить в модальном окне
+    const modalButtonConfirm = document.querySelector(".delete-modal__confirm-button");
+    
     modalButtonConfirm.addEventListener("click", () => {
-      let taskIndex = tasks.findIndex((el) => el.id === dataTaskId);
-      tasks.splice(taskIndex, 1);
-      chosenTask.remove();
-      modalDelete.classList.add("modal-overlay_hidden");
+      let taskIndex = tasks.findIndex((el) => el.id === dataTaskId); //получения индекса задачи в массиве
+      tasks.splice(taskIndex, 1); //удаление задачи из массива по индексу
+      chosenTask.remove(); 
+      modalDelete.classList.add("modal-overlay_hidden"); //скрываем модальное окно
     });
   }
 });
 
+
+//Смена светлой и темной темы по нажатию на Tab
 const taskItems = document.querySelectorAll(".task-item");
 const allButtons = document.querySelectorAll("button");
-let x = 0;
+let isTheme = 0;
 
 const keyTab = document.addEventListener("keydown", (event) => {
   if (event.key === "Tab") {
-    if (x === 1) {
+    if (isTheme === 1) {
       setThemeLight();
     } else {
       setThemeDark();
@@ -164,12 +182,12 @@ const setThemeDark = () => {
   document.body.style = "background: #24292E";
   taskItems.forEach((el) => (el.style = "color: #ffffff"));
   allButtons.forEach((el) => (el.style = "border: 1px solid #ffffff"));
-  return (x = 1);
+  return (isTheme = 1);
 };
 
 const setThemeLight = () => {
   document.body.style = "background: initial";
   taskItems.forEach((el) => (el.style = "color: initial"));
   allButtons.forEach((el) => (el.style = "border: none"));
-  return (x = 0);
+  return (isTheme = 0);
 };
